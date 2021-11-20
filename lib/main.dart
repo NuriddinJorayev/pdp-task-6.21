3bee3298ee09171422ab61daeb345dae3b33d938
Mobile Developer <jorayevnuriddin00@gmail.com>
2021-11-21 01:48:44 +0500

    qo'shildi

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:language1/pages/home_page.dart';
 
+// more
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
